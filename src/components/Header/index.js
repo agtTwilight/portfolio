238941afa5439968d282eaee8be3e5efdf206853
agtTwilight <lukasmacmillen@gmail.com>
2023-04-20 11:08:52 -0700

    added mobile nav menu button functionality

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,23 @@
+import menu from "./assets/menu.png"
 import React from "react";
 import resume from "./assets/resume.pdf";
 import "./style.css";
 
 export const Header = () => {
+    async function displayNav() {
+        const nav = document.querySelector("#nav-links");
+        nav.setAttribute("style", "transform: translateX(0);");
+        hideNav(nav);
+    }
+
+    function hideNav(nav) {
+        document.addEventListener("click", (e) => {
+            if(!nav.contains(e.target) && e.target !== document.querySelector("#nav-toggle")) {
+                nav.setAttribute("style", "transform: translateX(100%);")
+            }
+        });
+    }
+
   return (
     <nav>
       <p>Lukas</p>
@@ -12,7 +27,7 @@ export const Header = () => {
         <a href="#Contact">Contact</a>
         <a id="resume" href={resume}>Resume</a>
       </div>
-      <button id="nav-toggle"></button>
+      <img id="nav-toggle" onClick={displayNav} src={menu} alt="hamburger button"></img>
     </nav>
   );
 };
