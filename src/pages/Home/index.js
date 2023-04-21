4ec5f6f1c446bde909d5da33c5bcd6e412fa5fb5
agtTwilight <lukasmacmillen@gmail.com>
2023-04-20 21:22:19 -0700

    fixed mobile z-index bug. Nav menu now on top of all elements

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,8 +6,8 @@ export const Home = () => {
     <section id='home-wrap'>
         <h2>Hi, I'm Lukas</h2>
         <section id='rotating-text-container'>
-            <h1 id='rotating-text'>Full-Stack Developer <br />
-                Back-End Specialist <br />
+            <h1 id='rotating-text'>Full-Stack Developer<br />
+                Back-End Specialist<br />
                 &amp;
                 Data Scientist
             </h1>
