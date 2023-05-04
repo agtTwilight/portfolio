584609f297beb985e7bb77e9791a945fabd2aff9
agtTwilight <lukasmacmillen@gmail.com>
2023-05-04 09:15:05 -0700

    added initial styling to contact form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,38 +1,44 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-// import "./style.css"
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./style.css";
 
 export const ContactForm = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-// TODO add .env file
-    emailjs.sendForm(
-        'service_zo7n8zv', 
-        'template_3qonnuo', 
-        form.current, 
-        'qAGcWm6fIZZLN4Dsa')
-      .then((result) => {
+    // TODO add .env file
+    emailjs
+      .sendForm(
+        "service_zo7n8zv",
+        "template_3qonnuo",
+        form.current,
+        "qAGcWm6fIZZLN4Dsa"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
           console.log("message sent");
           e.target.reset();
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   return (
-    <form id='contactForm' ref={form} onSubmit={sendEmail}>
-      <label>Your Name:</label>
-      <input type="text" name="user_name" />
-      <label>Your Email Address:</label>
-      <input type="email" name="user_email"/>
-      <label>What would you like to say?</label>
-      <textarea name="message" />
-      <input id='contactBtn' type="submit" value="Send" />
+    <form id="contactForm" ref={form} onSubmit={sendEmail}>
+      <section id="contactInputs">
+        <div>
+          <input type="text" name="user_name" placeholder="Name" />
+          <input type="email" name="user_email" placeholder="Email" />
+        </div>
+        <textarea name="message" placeholder="Type your message here" />
+      </section>
+      <input id="contactBtn" type="submit" value="Submit" />
     </form>
   );
 };
 
-export default ContactForm
+export default ContactForm;
