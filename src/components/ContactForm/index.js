67f681a56e811f98532015a04ae89d8fe5c94c88
agtTwilight <lukasmacmillen@gmail.com>
2023-05-09 16:24:29 -0700

    added contact form closure on external click for desktop view. Also added some hover animations to desktop components.

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,6 +1,6 @@
 import emailjs from "@emailjs/browser";
-import exit from "../../components/Header/assets/exit.png"
-import React, { useRef } from "react";
+import exit from "../../components/Header/assets/exit.png";
+import React, { useEffect, useRef } from "react";
 import "./style.css";
 
 export const ContactForm = () => {
@@ -28,25 +28,44 @@ export const ContactForm = () => {
       );
   };
 
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+  }, []);
+
+  const refOne = useRef(null);
+
+  const handleClickOutside = (e) => {
+    if (!refOne.current.contains(e.target) && document.querySelector("#contactForm").style.transform && window.screen.width > 999) {
+      hideForm();
+    }
+  };
+
   function hideForm() {
     document
-        .querySelector('#contactForm')
-        .setAttribute('style', 'transform: translateX(100);');
+      .querySelector("#contactForm")
+      .setAttribute("style", "transform: translateX(100);");
   }
 
   return (
-    <form id="contactForm" ref={form} onSubmit={sendEmail}>
-        <img id="form-close-desktop" onClick={hideForm} src={exit} alt="An X to close the nav on mobile"></img>
+    <section id="form-ref" ref={refOne}>
+      <form id="contactForm" ref={form} onSubmit={sendEmail}>
+        <img
+          id="form-close-desktop"
+          onClick={hideForm}
+          src={exit}
+          alt="An X to close the nav on mobile"
+        ></img>
         <h2>Get In Touch!</h2>
-      <section id="contactInputs">
-        <div>
-          <input type="text" name="user_name" placeholder="Name" />
-          <input type="email" name="user_email" placeholder="Email" />
-        </div>
-        <textarea name="message" placeholder="Type your message here" />
-      </section>
-      <input id="contactBtn" type="submit" value="Submit"/>
-    </form>
+        <section id="contactInputs">
+          <div>
+            <input type="text" name="user_name" placeholder="Name" />
+            <input type="email" name="user_email" placeholder="Email" />
+          </div>
+          <textarea name="message" placeholder="Type your message here" />
+        </section>
+        <input id="contactBtn" type="submit" value="Submit" />
+      </form>
+    </section>
   );
 };
 
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -3,19 +3,7 @@ import React from "react";
 import "./style.css";
 
 export const Contact = (props) => {
-    // TODO make a function that closes the contact form when user clicks outside of open form
-    // if(window.screen.width >= 1000) {
-    //     let count = 0
-    //     document.addEventListener("click", (e) => {
-    //         if(document.querySelector("#contactForm").style.transform && count === 1) {
-    //             document.querySelector("#contactForm").setAttribute('style', 'transform: translateX(100);');
-    //             count = 0
-    //         } else if(document.querySelector("#contactForm").style.transform) {
-    //             count ++
-    //         }
-    //         console.log(count)
-    //     }, {once:true}) 
-    // }
+
   return (
     <section id="contact-section">
       <h2>What's Next?</h2>
