a73f0466b36c6418dc2134aaa5590e803cf0f7c9
agtTwilight <lukasmacmillen@gmail.com>
2023-06-21 14:55:41 -0700

    styled highlight project cards

--- src/components/HighlightProject/index.js
+++ src/components/HighlightProject/index.js
@@ -8,13 +8,15 @@ export const HighlightProject = () => {
 		<section className="highlight-card">
 			<div className="highlight-content">
 				<h2 className="highlight-title">Projct Title</h2>
-				<p>A brief description of the project</p>
-				<ul className="tech-list">
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>JavScript</li>
-				</ul>
+				<p>
+					A brief description of the project. When text wraps it looks like this
+				</p>
 			</div>
+			<ul className="tech-list">
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>JavScript</li>
+			</ul>
 			<div className="highlight-links">
 				<a>
 					<img src={github} alt="the github logo"></img>
